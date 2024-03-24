e48f75b5563934b21545bb90998282a3f60efdc1
Oswald David Gutiérrez <davidgcortina@gmail.com>
2024-03-24 03:49:36 -0500

    Se entrega el puesto correctamente

--- src/main/java/hotelP1/LecturaDatos.java
+++ src/main/java/hotelP1/LecturaDatos.java
@@ -9,30 +9,7 @@ import java.util.Scanner;
 public class LecturaDatos {
     
     Scanner entrada = new Scanner (System.in);
-
-    public String leerString(String mensaje){
-        System.out.println(mensaje+": ");
-        return entrada.nextLine() ;
-    }
     
-    public int leerInt(String mensaje){
-        System.out.println(mensaje+": ");
-        int dato = entrada.nextInt();
-        return dato;
-    }
-
-    public float leerFloat(String mensaje){
-        System.out.println(mensaje);
-        float datoFloat = entrada.nextFloat();
-        return datoFloat;
-    }
-
-    public double leerDouble(String mensaje){
-        System.out.println(mensaje);
-        double datoDouble = entrada.nextDouble();
-        return datoDouble;
-    }
-
     public int leeryValidarInt(String mensaje) {
         int dato;
         do {
